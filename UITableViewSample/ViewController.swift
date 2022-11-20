70250b53775bc52983401b8b5ba34f27592d5abf
tianbo <tianbo@hey.com>
2022-11-19 22:40:17 -0800

    Initial Commit

---
+++ UITableViewSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewSample
+//
+//  Created by Tianbo Qiu on 11/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

4317c4fbea67b173af93fc08163596ec6561a393
tianb0 <tianbo@hey.com>
2022-12-20 09:26:34 -0800

    Codable

--- NavigationView/Item.swift
+++ NavigationView/Item.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class Item: Equatable {
+class Item: Equatable, Codable {
     var name: String
     var value: Int
     var uid: String
--- NavigationView/ItemStore.swift
+++ NavigationView/ItemStore.swift
@@ -11,11 +11,32 @@ import UIKit
 class ItemStore {
     var items = [Item]()
     
+    let itemArchiveURL: URL = {
+        let documentDirs = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDir = documentDirs.first!
+        return documentDir.appendingPathComponent("items.plist")
+    }()
+    
     // Init with a few random items
     init() {
-        for _ in 0..<10 {
-            createOne()
+        do {
+            let data = try Data(contentsOf: itemArchiveURL)
+            let unarchiver = PropertyListDecoder()
+            let items = try unarchiver.decode([Item].self, from: data)
+            self.items = items
+        } catch {
+            print("Error reading in saved items: \(error)")
         }
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(saveChanges),
+                                       name: UIScene.didEnterBackgroundNotification,
+                                       object: nil)
+        
+//        for _ in 0..<10 {
+//            createOne()
+//        }
     }
     
     // Creates a random item
@@ -40,5 +61,19 @@ class ItemStore {
         
         let item = items.remove(at: fromIndex)
         items.insert(item, at: toIndex)
+    }
+    
+    @objc func saveChanges() -> Bool {
+        print("Saving items to \(itemArchiveURL)")
+        do {
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(items)
+            try data.write(to: itemArchiveURL, options: [.atomic])
+            print("Saved all of the items")
+            return true
+        } catch let encodingError {
+            print("Error encoding items: \(encodingError)")
+            return false
+        }
     }
 }
